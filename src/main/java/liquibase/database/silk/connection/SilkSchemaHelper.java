a7fc3445af17f334591273978194ea0091a23525
Mark <mverleg.noreply@gmail.com>
2020-05-11 23:09:42 +0200

    File system locking looks ok, might be moved to Silk itself

--- src/main/java/liquibase/database/silk/connection/SilkSchemaHelper.java
+++ src/main/java/liquibase/database/silk/connection/SilkSchemaHelper.java
@@ -11,16 +11,50 @@ import org.apache.commons.lang3.NotImplementedException;
 
 import nl.markv.silk.types.SilkSchema;
 
-import static org.apache.commons.lang3.Validate.isTrue;
-
+//TODO @mark: also add tsChanged checks (for the non-locking variant)
 public class SilkSchemaHelper {
 
-	//TODO @mark: do something with file-system locking to prevent race conditions
+	public static class JsonSchemaFile {
+		@Nonnull
+		public final SilkSchema schema;
+		@Nonnull
+		final Path schemaPath;
+		boolean locked;
 
-	@Nonnull
-	@CheckReturnValue
-	public static SilkSchema loadWithoutLock(@Nonnull Path path) {
-		throw new NotImplementedException("todo: ");  //TODO @mark: implement
+		public JsonSchemaFile(@Nonnull SilkSchema schema, @Nonnull Path schemaPath, boolean locked) {
+			this.schema = schema;
+			this.schemaPath = schemaPath;
+			this.locked = locked;
+		}
+
+		/**
+		 * Save the changes, and unlock the schema (if it was locked).
+		 */
+		public void saveAndUnlock() {
+			if (!locked) {
+				// Wait for locks by other processes to release.
+				waitForLockFileOrFail(lockFilePath(schemaPath));
+			}
+			try {
+				directlySave(schema, schemaPath);
+			} finally {
+				if (locked) {
+					locked = false;
+					unlock(schemaPath);
+				}
+			}
+		}
+
+		/**
+		 * Save the changed, hanging on to the lock (if any) for possible further changes.
+		 */
+		public void saveWithoutUnlock() {
+			if (!locked) {
+				// Wait for locks by other processes to release.
+				waitForLockFileOrFail(lockFilePath(schemaPath));
+			}
+			directlySave(schema, schemaPath);
+		}
 	}
 
 	/**
@@ -28,23 +62,35 @@ public class SilkSchemaHelper {
 	 */
 	@Nonnull
 	@CheckReturnValue
-	public static SilkSchema loadAndLock(@Nonnull Path path) {
+	public static JsonSchemaFile loadWithoutLock(@Nonnull Path path) {
+		return new JsonSchemaFile(directlyLoad(path), path, false);
+	}
+
+	/**
+	 * Load the Silk schema. If it is locked, waits a while, and fails if lock cannot be obtained.
+	 */
+	@Nonnull
+	@CheckReturnValue
+	public static JsonSchemaFile lockAndLoad(@Nonnull Path path) {
 		lock(path);
+		SilkSchema schema;
 		try {
-			return loadWithoutLock(path);
+			schema = directlyLoad(path);
 		} catch (Exception ex) {
 			unlock(path);
 			throw ex;
 		}
+		return new JsonSchemaFile(schema, path, true);
 	}
 
-	public static void saveAndUnlock(@Nonnull SilkSchema silkSchema, @Nonnull Path path) {
-		saveWithoutUnlock(silkSchema, path);
-		unlock(path);
+	@Nonnull
+	@CheckReturnValue
+	private static SilkSchema directlyLoad(@Nonnull Path schemaPath) {
+		throw new NotImplementedException("todo");  //TODO @mark: implement
 	}
 
-	public static void saveWithoutUnlock(@Nonnull SilkSchema silkSchema, @Nonnull Path path) {
-		throw new NotImplementedException("todo: ");  //TODO @mark: implement
+	private static void directlySave(@Nonnull SilkSchema silkSchema, @Nonnull Path schemaPath) {
+		throw new NotImplementedException("todo");  //TODO @mark: implement
 	}
 
 	public static void lock(@Nonnull Path schemaPath) {
@@ -52,12 +98,7 @@ public class SilkSchemaHelper {
 			throw new IllegalStateException("Could not lock schema at '" + schemaPath + "' because it does not exist");
 		}
 		Path lockFile = lockFilePath(schemaPath);
-		if (!waitForLockFile(lockFile, 30_000)) {
-			throw new IllegalStateException("Could not lock Silk schema at '" + schemaPath +
-					"' because it is already locked, and was not released quickly enough " +
-					"(if you are sure that no other program uses the schema, delete the " +
-					"lock file at '" + lockFilePath(schemaPath) + "')");
-		}
+		waitForLockFileOrFail(lockFile);
 		try {
 			Files.createFile(lockFile);
 		} catch (IOException ex) {
@@ -65,8 +106,17 @@ public class SilkSchemaHelper {
 		}
 	}
 
-	public static void unlock(@Nonnull Path path) {
-		throw new NotImplementedException("todo: ");  //TODO @mark: implement
+	private static void unlock(@Nonnull Path schemaPath) {
+		Path lockFile = lockFilePath(schemaPath);
+		if (!Files.exists(lockFile)) {
+			throw new IllegalStateException("Tried to unlock file '" + schemaPath +
+					"', but the file was not locked");
+		}
+		try {
+			Files.delete(lockFile);
+		} catch (IOException ex) {
+			throw new IllegalStateException(ex);
+		}
 	}
 
 	/**
@@ -74,12 +124,12 @@ public class SilkSchemaHelper {
 	 *
 	 * @return Whether the file has disappeared before the time expired.
 	 */
-	private static boolean waitForLockFile(@Nonnull Path path, int timeoutMs) {
-		if (!Files.exists(path)) {
+	private static boolean waitForLockFile(@Nonnull Path lockFilePath, int timeoutMs) {
+		if (!Files.exists(lockFilePath)) {
 			return true;
 		}
-		System.out.println("waiting for lock file to disappear (" + path +
-				") for at most " + (timeoutMs / 1000) + "s";
+		System.out.println("waiting for lock file to disappear (" + lockFilePath +
+				") for at most " + (timeoutMs / 1000) + "s");
 		int waitTimeMs = 0;
 		while (waitTimeMs < timeoutMs) {
 			waitTimeMs += 50;
@@ -88,13 +138,27 @@ public class SilkSchemaHelper {
 			} catch (InterruptedException ex) {
 				// Someone wants us to wake up; we'll just skip this tick.
 			}
-			if (!Files.exists(path)) {
+			if (!Files.exists(lockFilePath)) {
 				return true;
 			}
 		}
 		return false;
 	}
 
+	/**
+	 * Like {@link #waitForLockFile(Path, int)}, but throws if the lock is not obtained in 30s.
+	 */
+	private static void waitForLockFileOrFail(@Nonnull Path schemaPath) {
+		Path lockPath = lockFilePath(schemaPath);
+		if (!waitForLockFile(lockPath, 30_000)) {
+			throw new IllegalStateException("Could not lock Silk schema at '" + schemaPath +
+					"' because it is already locked, and was not released quickly enough " +
+					"(if you are sure that no other program uses the schema, delete the " +
+					"lock file at '" + lockPath + "')");
+		}
+	}
+
+	@Nonnull
 	private static Path lockFilePath(@Nonnull Path schemaPath) {
 		return schemaPath.resolveSibling(schemaPath.getFileName() + ".lock~");
 	}
